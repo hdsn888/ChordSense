bac4068f50ac4df9d288e5c42569051b3fdde6da
hdsn888 <hdsn888@gmail.com>
2025-07-15 00:44:31 +0000

    Client-server connection registered
    Servers interacting properly

---
+++ Backend/back.cjs
@@ -0,0 +1,59 @@
+
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+app.use(cors( {
+    origin: /^http:\/\/localhost:\d+$/,
+    credentials: true
+}));
+
+let con = mysql.createConnection ({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Doremi88!!",
+    database: "chords_data"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+})
+
+app.get('/chords/:chordName', (req, res) => {
+    console.log("Request received");
+    const chord = req.params.chordName;
+    const sql = `SELECT Link FROM chords
+            WHERE Concert_Pitch = ?`;
+    console.log("About to query")
+    con.query(sql, [chord], function(err, result) {
+        if (err) {
+            res.status(500).json({error: "Database error"});
+            return;
+        } if (result.length === 0) {
+            res.status(404).json({error: "Chord not found"});
+            return;
+        }
+        res.json(result);
+    });
+
+});
+ // const sql = `SELECT Link FROM chords
+    //     WHERE Concert_Pitch = \"CMaj\" `;
+    // con.query(sql, function(err, result) {
+    //     if (err) throw err;
+    //     console.log(result[0].Link);
+    //     const audio = new Audio(result[0].Link);
+    //     audio.play();
+    // });
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,72 @@
 import { useState } from 'react'
-import './App.css'
+import axios from 'axios';
+// const apiCall = async () => {
+//  try {
+//   await axios.get('http://localhost:3001/');
+//   console.log("apiCall is functioning");
+//   return 0;
+//  } catch (error) {
+//     console.log(error);
+//     return 1;
+//  }
+// }
+
+const apiCall = async() => {
+  try {
+    console.log("About to send request and fetch response");
+    const promise = await fetch("http://localhost:3001/chords/CMaj");
+    console.log("Promise received");
+    // if (!promise.ok) {
+    //   throw new Error("");
+    // }
+    const chord = await promise.json();
+    console.log(chord[0].Link);
+    const audio = new Audio (chord[0].Link);
+    audio.play();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function MyButton({count, onClick}) {
   return (
-    <button onClick = {onClick}>
+    <button onClick = {e => { 
+      e.stopPropagation();
+      onClick();
+      }}>
       Clicked {count} times
     </button>
-  )
+  );
 }
 
-export default function App() {
-  const [count, setCount] = useState(0); // Hook 
+function Header() {
+  return (
+    <div>
+      <h1>ChordSense</h1>
+      <h4>Music theory explained simply</h4>
+    </div>
+  );
+}
+
+function ChordList() {
+  let listChords = chords.map(chord => <li key = {chord.id}> {chord.name} </li>);
+  return <ul>{listChords}</ul>;
+}
+
+export default function App() { // file can only have one default export
+  const [count, setCount] = useState(0); // Hook
+  console.log("App is rendering");
 
   function handleClick() {
     setCount(count + 1);
   }
-
   // props - information passed from its parent component
   return (
     <div>
-      <h1>Counters that update together</h1>
-      <MyButton count = {count} onClick = {handleClick}/>
-      <MyButton count = {count} onClick = {handleClick}/>
+      <Header />
+      <ChordList />
+      <MyButton count = {count} onClick = {handleClick} />
+      <button onClick = {apiCall}>CMaj Chord</button>
     </div>
   );
 }
