cd130fc2cc38d97b2312e363878a0aea88f0876f
hdsn888 <hdsn888@gmail.com>
2025-08-21 03:29:40 +0000

    Implemented chord display function
    Full transition from buttons to interactive measure

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,19 @@ const notePositions = new Map([
   ['C4', {cy: 66.375, xPath: 11.8, yPathStart: 63.35, yPathEnd: 5.35}],
   ['D4', {cy: 55.35, xPath: 11.8, yPathStart: 52.325, yPathEnd: -5.675}],
   ['E4', {cy: 44.325, xPath: 11.8, yPathStart: 41.3, yPathEnd: -16.7}],
+  ['F#4', {cy: 33.3, xPath: 11.8, yPathStart: 30.05, yPathEnd: -27.725}],
   ['G4', {cy: 22.275, xPath: 11.8, yPathStart: 19.25, yPathEnd: -38.75}],
-  ['B4', {cy: -0.225, xPath: -11.6, yPathStart: -1.025, yPathEnd: 59.025}],
-  ['C5', {cy: -10.8, xPath:-11.6, yPathStart: -10, yPathEnd: 48}]
+  ['A4', {cy: 11.25, xPath: 11.8, yPathStart: 8.225, yPathEnd: -49.775}],
+  ['B4', {cy: 0.225, xPath: -11.6, yPathStart: -1.025, yPathEnd: 59.025}],
+  ['C5', {cy: -10.8, xPath:-11.6, yPathStart: -10, yPathEnd: 48}],
+  ['C#5', {cy: -10.8, xPath:-11.6, yPathStart: -10, yPathEnd: 48}],
+  ['D5', {cy: -21.825, xPath: -11.6, yPathStart: -21.025, yPathEnd: 36.975}],
+  ['D#5', {cy: -21.825, xPath: -11.6, yPathStart: -21.025, yPathEnd: 36.975}],
+  ['E5', {cy: -32.85, xPath: -11.6, yPathStart: -32.05, yPathEnd: 25.95}],
+  ['F5', {cy: -43.875, xPath: -11.6, yPathStart: -43.075, yPathEnd: 14.925}],
+  ['F#5', {cy: -43.875, xPath: -11.6, yPathStart: -43.075, yPathEnd: 14.925}],
+
+
 
 ]);
 
@@ -48,32 +58,34 @@ const playNote = async (note) => {
   sampler.triggerAttackRelease(note, 2.5);
 }
 
-//Sets buttons to correct chord
-const ChordButtons = ({chords, chord}) => {
-  return (
-    <div className = "flex flex-row shrink-0" >
-      {chords.get(chord).notes.map((note) => (
-        <div key = {note} className = "w-40 m-2">
-          <button className = "object-cover btn-sm" onClick = {() => playNote(note)}>
-            <img src = {`/${note}.png`} className = "w-32 h-20" />  {/* w-32 h-20*/}
-          </button>
-            <p className = "p-5">{note}</p>
-        </div>
-      ))}
-      <div className = "w-40 m-2">
-        <button className = "object-cover btn-sm" onClick = {() => playChord({chords, chord})}>
-          <img src = {`/${chord}.png`} className = "w-36 h-20" />
-        </button>
-          <p className = "p-5">{chord}</p>
-      </div>
+// //Sets buttons to correct chord
+// const ChordButtons = ({chords, chord}) => {
+//   return (
+//     <div className = "flex flex-row shrink-0" >
+//       {chords.get(chord).notes.map((note) => (
+//         <div key = {note} className = "w-40 m-2">
+//           <button className = "object-cover btn-sm" onClick = {() => playNote(note)}>
+//             <img src = {`/${note}.png`} className = "w-32 h-20" />  {/* w-32 h-20*/}
+//           </button>
+//             <p className = "p-5">{note}</p>
+//         </div>
+//       ))}
+//       <div className = "w-40 m-2">
+//         <button className = "object-cover btn-sm" onClick = {() => playChord({chords, chord})}>
+//           <img src = {`/${chord}.png`} className = "w-36 h-20" />
+//         </button>
+//           <p className = "p-5">{chord}</p>
+//       </div>
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
 const ChordDisplay = ({chords, chord}) => {
   return (
-   <svg className = "w-full h-[300px] min-w-[400px] my-10 mx-auto block" viewBox = "-500 -100 1000 300">
+   <div>
+    <h2 className = "mx-auto mt-15 mb-5 text-3xl">{chord} Chord</h2>
+    <svg className = "w-full h-[300px] min-w-[400px] mx-auto block mb-20" viewBox = "-500 -100 1000 300">
       <rect
         x = "-500"
         y = "-100"
@@ -90,8 +102,11 @@ const ChordDisplay = ({chords, chord}) => {
       {chords.get(chord).notes.map((note, index) => (
         <svg key = {note} className = "group" x={-250 + (index*100)} y="-100" width="100" height="300" viewBox="-50 -100 100 300" 
       onClick={() => playNote(note)} >
-        <text className = "fill-black group-hover:fill-red-500" x = "-10" y = "150">{note}</text>
+        <text className = "fill-black group-hover:fill-red-500" x = "-5" y = "130">{1+index*2}</text>
+        <text className = "text-lg font-bold fill-black group-hover:fill-red-500" x = "-10" y = "150">{note}</text>
         <rect x="-50" y="-100" width="100" height="200" fill="transparent" />
+        <LedgerLines note ={note} />
+        <Accidental note = {note} />
         <ellipse className = "fill-black group-hover:fill-red-500" rx="14" ry="10.3" cx="0" cy={notePositions.get(note).cy} transform={`rotate(-28 0 ${notePositions.get(note).cy})`}/>
         <path
           className = "stroke-black group-hover:stroke-red-500"
@@ -102,10 +117,13 @@ const ChordDisplay = ({chords, chord}) => {
       ))}
 
       <svg className = "group" x = {-250 + chords.get(chord).notes.length*100} y="-100" width="100" height="300" viewBox="-50 -100 100 300" onClick = {() => playChord({chords, chord})} >
-        <text className = "fill-black group-hover:fill-red-500" x = "-10" y = "150">{chord}</text>
+        <text className = "text-lg font-bold fill-black group-hover:fill-red-500" x = "-10" y = "150">{chord}</text>
         <rect x="-50" y="-100" width="100" height="200" fill="transparent" />
         {chords.get(chord).notes.map((note, index) => (
-          <ellipse key = {note} className = "fill-black group-hover:fill-red-500" rx="14" ry="10.3" cx="0" cy={notePositions.get(note).cy} transform={`rotate(-28 0 ${notePositions.get(note).cy})`}/>
+            <LedgerLines key = {note} note = {note} />
+        ))}
+        {chords.get(chord).notes.map((note, index) => (
+            <ellipse key = {note} className = "fill-black group-hover:fill-red-500" rx="14" ry="10.3" cx="0" cy={notePositions.get(note).cy} transform={`rotate(-28 0 ${notePositions.get(note).cy})`}/>
         ))}
         <path
           className = "stroke-black group-hover:stroke-red-500"
@@ -114,10 +132,52 @@ const ChordDisplay = ({chords, chord}) => {
         />
       </svg>
       
-
-      
     </svg>
+   </div>
   );
+}
+
+const LedgerLines = ({note}) => {
+  const ledgerLineNotes = ['C4', 'A5', 'B5', 'C6']
+  const validLedger = ledgerLineNotes.includes(note);
+  let d;
+  if (!validLedger) return;
+  switch (note) {
+    case 'C4': 
+      d = "M -20 66 L 20 66";
+      break;
+    case 'A5':
+    case 'B5':
+      d = "M -20 -66 L 20 -66"
+      break;
+    case 'C6':
+      d = "M -20 -66 L 20 -66 M -20 -88 L 20 -88"
+      break;
+  }
+  return <path className = "stroke-black" d = {d} strokeWidth = "1.5"/>
+}
+
+const Accidental = ({note}) => {
+  if (!note.includes("#") && !note.includes("b")) return;
+  if (note.includes("#")) {
+    return (
+      <>
+        <path
+          className = "stroke-black group-hover:stroke-red-500"
+          d = {`M -24 ${notePositions.get(note).cy - 29.2} L -24 ${notePositions.get(note).cy + 25.8} M -36 ${notePositions.get(note).cy - 26.2} L -36 ${notePositions.get(note).cy + 28.8}`}
+          strokeWidth="3"
+        />
+        <polygon 
+          className = "stroke-black fill-black group-hover:stroke-red-500 group-hover:fill-red-500"
+          points={`-40,${notePositions.get(note).cy - 11.025} -20,${notePositions.get(note).cy - 17.025} -20,${notePositions.get(note).cy - 9.025} -40,${notePositions.get(note).cy - 3.025}`}
+        />
+        <polygon
+          className = "stroke-black fill-black group-hover:stroke-red-500 group-hover:fill-red-500"
+          points={`-40,${notePositions.get(note).cy + 8.8} -20,${notePositions.get(note).cy + 2.8} -20,${notePositions.get(note).cy + 11.025} -40,${notePositions.get(note).cy + 16.8}`}
+        />
+      </>
+    );
+  }
 }
 
 // remember to pass all parent states as props to children
@@ -134,7 +194,7 @@ const RootDropdown = ({setIsShowing, setCurrChord, chords}) => {
     <div>
       <div className = "dropdown dropdown-bottom ">
       <div tabIndex = {0} role = "button" className = "p-6 w-xs btn btn-soft ">
-        Chord
+        Chords
       </div>
       <ul tabIndex = {0} className = "dropdown-content menu bg-base-100 rounded-box z-1 w-40 p-1 shadow-sm">
         {[...chords].map(([chord_name, notes]) => (
@@ -199,11 +259,10 @@ function MyButton({count, onClick}) {
   );
 }
 
-function Header() {
+function Instructions() {
   return (
-    <div className = "object-scale-down text-center">
-      <h1 className = "m-2 text-2xl">ChordSense</h1>
-      <h4 className = "text-sm">Music theory explained simply</h4>
+    <div className = "text-center my-10">
+      <h3>Welcome to ChordSense, the chord vocalization app! <br /> Select any chord from the "Chords" dropdown menu, and <br /> click the notes and chord in the measure to hear the chord's pitches.</h3>
     </div>
   );
 }
@@ -235,7 +294,6 @@ export default function App() { // file can only have one default export
     <div className = "min-h-screen flex flex-col bg-[#313945]">
       <header className = "shadow-lg px-5 py-5 flex justify-between">
           <div className = "max-w-4xl mx-auto flex justify-between justify-left items-center">
-            {/* <Header /> */}
             <img src="Logo.png" className = "w-40 h-10"/>
           </div>
           <div className = "flex mx-auto justify-right items-center">
@@ -243,10 +301,10 @@ export default function App() { // file can only have one default export
           </div>
       </header>
       <main className = "flex-1 px-4 px-12">
+        <Instructions />
         <div className = "max-w-4xl mx-auto text-center">
           {isReady && <RootDropdown setIsShowing = {setIsShowing} 
           setCurrChord = {setCurrChord} chords = {chords}/>}
-          {/* {isShowing && currChord && <ChordButtons chords = {chords} chord = {currChord} />} */}
           {isShowing && currChord && <ChordDisplay chords = {chords} chord = {currChord} /> }
         </div>
        
